3b115c5fb1c114029fdbd92fe79cb29ad5938556
Sergey Rymsha <rymsha@yandex.ru>
2019-10-04 13:25:39 +0200

    Find an alternative for ES Transport protocol #7391 make tasks work as well

--- modules/core/core-task/src/main/java/com/enonic/xp/impl/task/cluster/TaskTransportResponseHandler.java
+++ modules/core/core-task/src/main/java/com/enonic/xp/impl/task/cluster/TaskTransportResponseHandler.java
@@ -1,11 +1,17 @@
 package com.enonic.xp.impl.task.cluster;
 
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.Callable;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceReference;
+
+import com.enonic.xp.impl.task.TaskManager;
+import com.enonic.xp.task.TaskInfo;
 
 public class TaskTransportResponseHandler
     implements Callable<TaskTransportResponse>, Serializable
@@ -20,18 +26,37 @@ public class TaskTransportResponseHandler
     @Override
     public TaskTransportResponse call()
     {
-        BundleContext bundleContext = FrameworkUtil.getBundle( TaskTransportRequestHandler.class ).
-            getBundleContext();
-        ServiceReference<TaskTransportRequestHandler> serviceReference = bundleContext.
-            getServiceReference( TaskTransportRequestHandler.class );
-        TaskTransportRequestHandler handler = bundleContext.getService( serviceReference );
+        BundleContext bundleContext = FrameworkUtil.getBundle( TaskManager.class ).getBundleContext();
+        ServiceReference<TaskManager> serviceReference = bundleContext.getServiceReference( TaskManager.class );
+        TaskManager handler = bundleContext.getService( serviceReference );
         try
         {
-            return handler.messageReceived( request );
+            return messageReceived( handler, request );
         }
         finally
         {
             bundleContext.ungetService( serviceReference );
         }
+    }
+
+    private static TaskTransportResponse messageReceived( final TaskManager taskManager, final TaskTransportRequest request )
+    {
+        final List<TaskInfo> taskInfos;
+        switch ( request.getType() )
+        {
+            case BY_ID:
+                taskInfos = Optional.ofNullable( taskManager.getTaskInfo( request.getTaskId() ) ).
+                    map( Collections::singletonList ).
+                    orElse( Collections.emptyList() );
+                break;
+            case RUNNING:
+                taskInfos = taskManager.getRunningTasks();
+                break;
+            default:
+                taskInfos = taskManager.getAllTasks();
+                break;
+        }
+
+        return new TaskTransportResponse( taskInfos );
     }
 }
